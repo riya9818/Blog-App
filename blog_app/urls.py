f206b2a8697a450b580c46516e66990746a3e72a
riya9818 <reeyabasnet999@gmail.com>
2025-09-03 20:34:15 +0545

    Login and Logout Implementation

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -6,4 +6,8 @@ from blog_app import views
 urlpatterns = [
     path("", views.post_list, name="post-list"),
     path("post/<int:pk>", views.post_detail, name="post-detail"),
+    path("draft-list/", views.draft_list, name="draft-list"),
+    path("draft-detail/<int:pk>/", views.draft_detail, name="draft-detail"),
+    
 ]
+
--- blog_app/views.py
+++ blog_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from blog_app.models import Post
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 def post_list(request):
     posts = Post.objects.filter(published_at__isnull=False).order_by("-published_at")
@@ -15,5 +16,23 @@ def post_detail(request, pk):
     return render(
         request,
         "post_detail.html",
+        {"post": post},
+    )
+
+@login_required
+def draft_list(request):
+    posts= Post.objects.filter(published_at__isnull=True)
+    return render(
+        request,
+        "draft_list.html",
+        {"posts":posts},
+    )
+
+@login_required
+def draft_detail(request, pk):
+    post = Post.objects.get(pk=pk, published_at__isnull=True)
+    return render(
+        request,
+        "draft_detail.html",
         {"post": post},
     )
