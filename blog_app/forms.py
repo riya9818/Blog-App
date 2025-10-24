ab4b4cda8d4943baf71c26d7f59dcb23a41fd61b
riya9818 <reeyabasnet999@gmail.com>
2025-10-24 23:32:47 +0545

    forms.py updated

--- blog_app/forms.py
+++ blog_app/forms.py
@@ -3,6 +3,10 @@ from django import forms
 
 from blog_app.models import Post
 
+
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
 class PostForm(forms.ModelForm):
     class Meta:
         model =Post
