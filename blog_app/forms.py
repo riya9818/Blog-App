c0b07fc26855beddd954f63137a6fd6b9c4da3b9
riya9818 <reeyabasnet999@gmail.com>
2025-10-24 23:32:19 +0545

    forms.py init

--- blog_app/forms.py
+++ blog_app/forms.py
@@ -7,3 +7,12 @@ class PostForm(forms.ModelForm):
     class Meta:
         model =Post
         fields={"title","content"}
+
+
+
+class SignupForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ["username", "email", "password1", "password2"]
