1618442ecf7f0d607ef9f15bd75f4206f240f83f
riya9818 <reeyabasnet999@gmail.com>
2025-10-31 21:34:21 +0545

    models updated to support image upload

--- blog_app/models.py
+++ blog_app/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Post(models.Model):
@@ -9,7 +9,7 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     published_at = models.DateTimeField(null=True, blank=True)
-
+    image = models.ImageField(upload_to='post_images/',blank=True, null=True)
 
     def __str__(self):
         return self.title
